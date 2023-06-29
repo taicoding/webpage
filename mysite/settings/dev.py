f6bb9c97eb0441d17531a06fc9b8cbc21f8ffcdc
taicoding <tai.delgadillo@gmail.com>
2023-06-28 23:00:41 -0400

    learning wagtail

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-21#489+t5u9xx6fn*!d87oyk35ad)5=jc7$-(0@ix^$l+4^-5*"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
